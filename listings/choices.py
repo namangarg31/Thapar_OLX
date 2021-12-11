1f69afdbdb841f8e85cfdb999d77fdd418a1db9f
naman <namangarg040@gmail.com>
2021-12-11 11:15:23 +0530

    first commit

---
+++ listings/choices.py
@@ -0,0 +1,41 @@
+category_choices = {
+  'Electronics': 'Electronics',
+  'Books': 'Books',
+  'Cycles' : 'Cycles',
+  'Mattresses' : 'Mattresses',
+  'Gadgets' : 'Gadgets',
+  'Projects' : 'Projects'
+  }
+
+price_choices = {
+  '1000':'₹1,000',
+  '2000':'₹2,000',
+  '5000':'₹5,000',
+  '10000':'₹10,000',
+  '20000':'₹20,000',
+  '50000':'₹50,000',
+  '100000':'₹100,000',
+  '200000':'₹200,000',
+  '500000':'₹500,000',
+  '1000000':'₹1M+',
+  
+}
+
+address_choices = {
+  'Hostel-A': 'Hostel-A',
+  'Hostel-B': 'Hostel-B',
+  'Hostel-C': 'Hostel-C',
+  'Hostel-D': 'Hostel-D',
+  'Hostel-E': 'Hostel-E',
+  'Hostel-F': 'Hostel-F',
+  'Hostel-G': 'Hostel-G',
+  'Hostel-H': 'Hostel-H',
+  'Hostel-I': 'Hostel-I',
+  'Hostel-J': 'Hostel-J',
+  'Hostel-K': 'Hostel-K',
+  'Hostel-L': 'Hostel-L',
+  'Hostel-M': 'Hostel-M',
+  'Hostel-N': 'Hostel-N',
+}
+
+
